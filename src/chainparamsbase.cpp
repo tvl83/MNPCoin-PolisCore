1591eeda843247e1802b4c214b94b1a3d4f703ed
Enrique Berrueta <quique93@gmail.com>
2017-12-19 17:35:23 -0500

    Revert

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 11112;
+        nRPCPort = 9998;
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 11114;
+        nRPCPort = 19998;
         strDataDir = "testnet3";
     }
 };
