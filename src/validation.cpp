17465e402a1b591216f422d9a9cf1874a1118374
Cronos <eabz@polispay.org>
2019-05-12 12:57:17 -0500

    Revert fee include on nMint

--- src/validation.cpp
+++ src/validation.cpp
@@ -2375,8 +2375,7 @@ static bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockInd
 
     // ppcoin: track money supply and mint amount info
     CAmount nMoneySupplyPrev = pindex->pprev ? pindex->pprev->nMoneySupply : 0;
-    int newFee = pindex->IsProofOfStake() ? nFees : 0;
-    pindex->nMoneySupply = nMoneySupplyPrev + (nValueOut - nValueIn + newFee);
+    pindex->nMoneySupply = nMoneySupplyPrev + nValueOut - nValueIn;
     pindex->nMint = pindex->nMoneySupply - nMoneySupplyPrev;
 
     int64_t nTime3 = GetTimeMicros(); nTimeConnect += nTime3 - nTime2;
