0992de014d45c1519774c3197b8c11ca70394239
Cronos <eabz@polispay.org>
2019-09-20 17:42:00 -0500

    Bump protocol

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70218;
+static const int PROTOCOL_VERSION = 70219;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -23,7 +23,7 @@ static const int GETHEADERS_VERSION = 70077;
 static const int MIN_PEER_PROTO_VERSION = 70217;
 
 //! disconnect from peers older than this proto version when DIP3 is activated via the BIP9 deployment
-static const int MIN_PEER_PROTO_VERSION_DIP3 = 70218;
+static const int MIN_PEER_PROTO_VERSION_DIP3 = 70219;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
